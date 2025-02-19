fe06f54e6d2ae3a38db9aab47fe9bf5004d0cc4d
mu93100 <muriel.ehlinger@lepoles.org>
2025-02-19 16:02:20 +0100

    journée de m.

--- javascript01/cours/exos/exFev19.js
+++ javascript01/cours/exos/exFev19.js
@@ -1,21 +1,39 @@
 //faire un event au click sur le bouton et afficher valeur de l'input ds console
 // non entre un nom qui s'affiche dans un tableau
-// 
 
-const input=document.getElementById("input")
-const button=document.getElementById("button")
-const valeur=input.value // ce qu'on renseigne ds l'input
-console.log("valeur de l'input", valeur) //normalement  ce qu'on ecrit se voit ds console log
+const input = document.getElementById("input")
+const button = document.getElementById("button")
+// console.log("valeur de l'input", valeur) //normalement  ce qu'on ecrit se voit ds console log
+input.value = "" //val rentrée dans input à mettre pour firefox
+let tableauMU = []
 
 button.addEventListener("click", function () {
-    if (valeur.typeOf!=="string"){
-        alert=
-        return
+    const valeur = input.value // ce qu'on renseigne ds l'input
+    // à mettre à l'int. de la Fo pour que ça prenne la valeur de ce qu'on rentre dans l'input
+    // ca prend la val de l'input après le click
 
+    if (valeur ==="") {
+        alert("rempli comme il faut")
+    } else {
+        console.log("valeur input : " + valeur);
+        tableauMU.push(valeur)
+        console.log("tableau mu : ", tableauMU);
+        // document.write("<table>i</table>")
+        // return      
     }
-    
+for (let i = 0; i < tableauMU.length; i++) {
+    // const i = (valeur)
+}
 })
-// for (let i = 0; i < array.length; i++) {
-//     // const element = array[i];
-    
-// }
+
+
+// 
+    // if (valeur !== "") {    // if (typeOf valeur!=="string"){ alert= ...
+    //     console.log(valeur);
+    //     input.value = ""
+
+    // } else {
+    //     alert("rempli comme il faut")
+    // }
+
+
